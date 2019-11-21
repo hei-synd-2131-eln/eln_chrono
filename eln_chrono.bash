7e1ba6af66764b07934547467560df1868347c49
Silvan Zahno <silvan.zahno@hevs.ch>
2019-11-21 14:08:37 +0100

    2019-11-21 14:08:04: Automatic Laboratory Update with deployLab.bash :shipit:

--- eln_chrono.bash
+++ eln_chrono.bash
@@ -8,7 +8,7 @@ design_name=`basename $0 .bash`
 design_directory=`dirname ${BASH_SOURCE[0]}`
 
 hdl_script_name="$design_directory/Scripts/hdlDesigner.bash"
-#export HEI_LIBS_DIR=/media/hei-students/SYND/Ele_2131/ELN/Labs/Libraries
+export HEI_LIBS_DIR=$design_directory/Libs
 
 verbose=1
 SEPARATOR='--------------------------------------------------------------------------------'
